8fff737f266b9e246d54fe9c57a5ac2d8f0c0655
yhalk <yhalk@PF2FK1NN>
2022-04-14 17:04:14 +0300

    ne wproject

---
+++ CheckNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi. Let's check wther your number is even)");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+                Console.WriteLine("The number is even");
+            else
+                Console.WriteLine("The number is odd");
+        }
+    }
+}
--- CheckNumber/Properties/AssemblyInfo.cs
+++ CheckNumber/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("123")]
+[assembly: AssemblyTitle("CheckNumber")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("123")]
+[assembly: AssemblyProduct("CheckNumber")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59c3ec23-090c-46f5-8cb9-9bc4cfd91d5b")]
+[assembly: Guid("35d2ab0e-7881-4e87-9053-4f105bb728c8")]
 
 // Version information for an assembly consists of the following four values:
 //
