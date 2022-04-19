a0dc7f2fffcea089a54a85cff4283c4ed9339696
yhalk <yhalk@PF2FK1NN>
2022-04-19 14:18:24 +0300

    qwerty

---
+++ Marks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi. What is your Role No?");
+            string roleNo = Console.ReadLine();
+            Console.WriteLine("Hi. What is your name?");
+            string studentName = Console.ReadLine();
+            Console.WriteLine("Hi. What is your marks in Physics? Please use value between 0-100");
+            int markPhysics = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hi. What is your marks in Chemistry? Please use value between 0-100");
+            int markChemistry = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hi. What is your marks in ComputerScience? Please use value between 0-100");
+            int markComputerScience = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Role No: " + roleNo);
+            Console.WriteLine("Name of Student: " + studentName);
+            Console.WriteLine("Marks in Physics: " + markPhysics);
+            Console.WriteLine("Marks in Chemistry: " + markChemistry);
+            Console.WriteLine("Marks in Computer Science: " + markComputerScience);
+            Console.WriteLine("Your Total: " + (markChemistry+markComputerScience+markPhysics));
+            Console.WriteLine("Percentage: " + (markChemistry + markComputerScience + markPhysics)*100/300 + "%");
+
+
+
+
+            //Console.WriteLine("Role no {0}, student anem {1}", roleNo,studentName);
+
+        }
+    }
+}
---
+++ ObjectsSquare/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! Let's calculate the area of objects . Type in: Triangle, Square or Rectangle");
+            var mathObject = Console.ReadLine();
+
+            switch (mathObject.ToLower()) { 
+            
+                case "triangle":
+                    Console.WriteLine("Type in lenght of a side 'a' and a height 'b' ");
+                    int sideAA = Convert.ToInt32(Console.ReadLine());
+                    int heightB = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The area of a triangle is: " + (0.5 * sideAA * heightB));
+                    break;
+                
+                case "square":
+                    Console.WriteLine("Type in lenght of a side 'a'");
+                    int sideSquare = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The area of a square is: " + (sideSquare * sideSquare));
+                    break;
+                case "rectangle":
+                    Console.WriteLine("Type in lenght of sides 'a' and 'b' ");
+                    int sideA = Convert.ToInt32(Console.ReadLine());
+                    int sideB = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The area of a rectangle is: " + (sideA * sideB));
+                    break;
+
+                default:
+                    Console.WriteLine("Please provide one of values: Triangle, Square or Rectangle");
+                  break;
+            
+
+            }
+        }
+    }
+}
