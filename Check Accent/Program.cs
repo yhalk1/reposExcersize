a3c64a433bdea7a4c6f80ae5216867e4ab370168
yhalk <yhalk@PF2FK1NN>
2022-04-25 19:14:13 +0300

    commit

---
+++ Check Accent/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Accent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Provide whatsapp message");
+
+            string[] hello = { "hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc" };
+            string[] accent = { "english", "italian", "french", "german", "spanish", "czech", "polish" };
+            var helloLenght = hello.Length;
+            var char1 = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < helloLenght; i++)
+            {
+                if (char1.Contains(hello[i]))
+                    Console.WriteLine("It's an {0} accent", accent[i]);
+                
+            }
+            
+            
+
+          
+        }
+    }
+}
