8376bf4b5e4250eab1d454d9c070fe084a7a3dc9
yhalk <yhalk@PF2FK1NN>
2022-06-22 00:41:25 +0300

    program

---
+++ DictionaryEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> person = new Dictionary<int, string>();
+            person.Add(1, "Jack");
+            person.Add(2, "Bill");
+            person.Add(3, "Drake");
+            person.Add(4, "John");
+            person.Add(5, "Adam");
+
+            Console.WriteLine("Please enter number from 1 to 5");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (person.ContainsKey(number))
+                Console.WriteLine("Corresponding name is " + person[number]);
+
+            else
+                Console.WriteLine("Corresponding name is not exist");
+
+        }
+    }
+}
